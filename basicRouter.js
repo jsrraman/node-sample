c8db1d1ee8648b8fbc2df03a1c38aae6e2629ef7
Rajaraman <rajaraman.ks@gmail.com>
2015-12-01 16:47:12 +0530

    Add GET/POST requests

--- basicRouter.js
+++ basicRouter.js
@@ -3,6 +3,40 @@
 const http = require('http');
 const url = require('url');
 
+let routes = {
+    'GET': {
+        '/': (req, res) => {
+            res.writeHead(200, {'Content-type': 'text/html'});
+            res.end('<h1> Hello Router </h1>');
+        },
+
+        '/api/getItems': (req, res) => {
+            res.writeHead(200, {'Content-type': 'application/json'});
+            res.end(JSON.stringify(req.queryParams));
+        }
+    },
+
+    'POST': {
+        '/api/getItems': (req, res) => {
+            let body = '';
+
+            req.on('data', data => {
+                body += data;
+            });
+
+            req.on('end', () => {
+                console.log(body);
+                res.end();
+            });
+        }
+    },
+
+    'NA': (req, res) => {
+        res.writeHead(404);
+        res.end('Content not found');
+    }
+}
+
 function router(req, res) {
     //console.log('Request route', req.url);
     //console.log('Request method', req.method);
@@ -10,9 +44,18 @@ function router(req, res) {
     // parseQueryString parameter in url.parse method should be true to return
     // the query as an object
     let baseURI = url.parse(req.url, true);
+    let resolveRoute = routes[req.method][baseURI.pathname];
 
-    res.writeHead(200, {'Content-type' : 'text-html'});
-    res.end('<h1>Hello Router</h1>');
+    if (resolveRoute != undefined) {
+        // Add query parameters to req object for easy access
+        req.queryParams = baseURI.query;
+        resolveRoute(req, res);
+    } else {
+        routes['NA'](req, res);
+    }
+
+    //res.writeHead(200, {'Content-type': 'text-html'});
+    //res.end('<h1>Hello Router</h1>');
 }
 
 http.createServer(router).listen(5000, () => {
