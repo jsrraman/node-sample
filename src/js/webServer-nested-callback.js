86b542f1413000ebea8d2b222514bde3f52ff8fb
Rajaraman <rajaraman.ks@gmail.com>
2015-12-04 20:05:05 +0530

    Add promisify version of web server

--- src/js/webServer-nested-callback.js
+++ src/js/webServer-nested-callback.js
@@ -6,32 +6,8 @@ const fs = require('fs');
 const path = require('path');
 
 let mimeTypes = {
-    '.html': 'text/html'
+    '.html' : 'text/html'
 };
-
-function fileAccess(filePath) {
-    return new Promise((resolve, reject) => {
-        fs.access(filePath, fs.F_OK, error => {
-            if (!error) {
-                resolve(filePath);
-            } else {
-                reject(error);
-            }
-        });
-    });
-}
-
-function fileReader(filePath) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, (error, content) => {
-            if (!error) {
-                resolve(content)
-            } else {
-                reject(error);
-            }
-        });
-    })
-}
 
 function webServer(req, res) {
     // parseQueryString parameter in url.parse method should be true to return
@@ -53,19 +29,29 @@ function webServer(req, res) {
     // Compute the actual file path of the html file to be served
     let filePath = rootHtmlFolder + relativePathName;
 
-    fileAccess(filePath)
-        .then(fileReader)
-        .then(content=> {
-            let contentType = mimeTypes[path.extname(filePath)];
-
-            res.writeHead(200, {'Content-type': contentType});
-            res.end(content, 'utf-8');
-        })
-        .catch(()=> {
+    // Check if the requested file exists
+    fs.access(filePath, fs.F_OK, error => {
+        if (!error) {
+            // File exists, read and serve the content
+            fs.readFile(filePath, (error, content) => {
+                if (!error) {
+                    // Resolve the content type
+                    // Serve the file from the buffer
+                    let contentType = mimeTypes[path.extname(filePath)];
+                    res.writeHead(200, {'Content-type': contentType});
+                    res.end(content, 'utf-8');
+                } else {
+                    // Serve a 500 error
+                    res.writeHead(500);
+                    res.end('The server could not read the file requested');
+                }
+            })
+        } else {
             // Serve a 404 error
             res.writeHead(404);
-            res.end('The server could not access the file requested');
-        });
+            res.end('File not found');
+        }
+    });
 }
 
 http.createServer(webServer).listen(5000, () => {
