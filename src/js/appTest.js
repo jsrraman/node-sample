9233690ec9d55c96d353b4d700ac874c8e4b791a
Rajaraman <rajaraman.ks@gmail.com>
2015-12-06 18:59:18 +0530

    Add sample code to illustrate exporting function vs object from a module

---
+++ src/js/appTest.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/*********************************
+ Call printMe() in Convention 1
+ **********************************/
+//const Test = require('./test');
+//const test = new Test();
+//
+//test.printMe();
+
+/*********************************
+ Call printMe() in Convention 2
+ **********************************/
+const test = require('./test');
+test.printMe();
---
+++ src/js/test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/******************************************************************
+ Illustration: exporting function vs object from a module
+ ******************************************************************/
+
+/******************************************************************
+ Convention 1
+
+ In the calling module (assume appTest.js in the same directory),
+ you should call like below
+
+ const Test = require('./test.js');
+ const test = new Test();
+
+ test.printMe();
+ ******************************************************************/
+
+/********************************************************************
+ This exports a function which returns an object with a property called printMe
+ which is of type function
+ ********************************************************************/
+//module.exports = function () {
+//    return {
+//        printMe: function () {
+//            console.log('hello world');
+//        }
+//    }
+//};
+
+/******************************************************************
+ Convention 2
+
+ In the calling module (assume appTest.js in the same directory),
+ you should call like the following
+
+ const test = require('./test.js');
+ test.printMe();
+ *****************************************************************/
+
+/********************************************************************
+ This exports an object with a property called printM which is of type function
+ ********************************************************************/
+module.exports = {
+    printMe: function () {
+        console.log('hello world');
+    }
+};
+
+/******************************************************************
+ Difference between Convention 1 and Convention 2
+ Note: This is my understanding. Need to revisit later if it is wrong
+
+ In Convention 1, we are exporting just a function, so to treat it like a true object
+ and call printMe function, you need to create an object like new require('./test')
+ and then call printMe on it.
+
+ In Convention 2, we are exporting an object itself, so you can directly call
+ printMe without saying new require('./test')
+
+ Prefer Convention 2 always for its simplicity
+ *****************************************************************/
